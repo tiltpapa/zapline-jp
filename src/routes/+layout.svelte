7736ec668ea3cb272ec786ad2d0f33496f9cbed1
tiltpapa <134381975+tiltpapa@users.noreply.github.com>
2024-05-14 19:25:14 +0900

    Add navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,3 +5,21 @@
 <svelte:head>
     <title>Zapline-JP</title>
 </svelte:head>
+
+<nav class="navbar navbar-expand navbar-light bg-light">
+    <a class="navbar-brand ps-3" href="/">Zapline-JP</a>
+    <ul class="nav navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled" href="/about">about</a>
+        </li>
+    </ul>
+</nav>
+
+<slot></slot>
+
+<style>
+
+</style>
