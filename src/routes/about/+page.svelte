669114d09c18d04377aadfefd51626044778c52e
tiltpapa <134381975+tiltpapa@users.noreply.github.com>
2024-05-22 15:42:36 +0900

    Add about

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,39 @@
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-8 offset-md-2">
+            <h1 class="mb-4">About Us</h1>
+            
+            <!-- 概要 Section -->
+            <div class="mb-4">
+                <p>Nostr日本ユーザーのZapの流れを観察するためのWebアプリ</p>
+                <p>Web App for watching Zap of Japan cluster on Nostr</p>
+            </div>
+            
+            <div class="mb-4">
+                <h3>Nostrとは</h3>
+                <p><a href="https://hello.nostrapp.me" target="_blank">Hello, Nostr! - Nostr日本語チュートリアルサイト</a></p>
+                <p><a href="https://welcome.nostr-jp.org" target="_blank">Welcome to Nostr! ～ Nostrをはじめよう！ ～</a></p>
+            </div>
+
+            <!-- Author Section -->
+            <div class="mb-4">
+                <h3>Author</h3>
+                <p><a href="https://nostter.app/tiltpapa.tv" target="_blank">ティル父さん / tiltpapa</a></p>
+            </div>
+            
+            <!-- Repository Link Section -->
+            <div class="mb-4">
+                <h3>Repository</h3>
+                <p><a href="https://github.com/tiltpapa/zapline-jp" target="_blank">https://github.com/tiltpapa/zapline-jp</a></p>
+            </div>
+            
+            <!-- Thanks Section -->
+            <!--
+            <div class="mb-4">
+                <h3>Thanks</h3>
+                <p>Special thanks to all the contributors and supporters.</p>
+            </div>
+            -->
+        </div>
+    </div>
+</div>
