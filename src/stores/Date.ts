b38eb62ae769f9e3bab127a72f0348c1d75a1f22
tiltpapa <134381975+tiltpapa@users.noreply.github.com>
2024-07-17 19:50:58 +0900

    Case: no old events

--- src/stores/Date.ts
+++ src/stores/Date.ts
@@ -3,3 +3,5 @@ import { derived, writable, type Readable, type Writable } from "svelte/store";
 
 export const untilDate: Writable<number> = writable(now());
 export const sinceDate: Readable<number> = derived(untilDate, $untilDate => $untilDate  - (3 * 60 * 60));
+
+export const lastUntilDate: Writable<number> = writable(0);
